9142396868ef1f0e462b3f918352263f5b79799f
lvvy <984723827@qq.com>
2018-08-18 10:01:38 +0800

    url

--- ac/urls.py
+++ ac/urls.py
@@ -25,7 +25,7 @@ from war.views import r_index
 #     url(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATICFILES_DIRS}),
 # ]
 
-urlpatterns = static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS) + static(settings.STATICFILES_DIRS,document_root=settings.MEDIA_ROOT)
+urlpatterns = static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
 
 
 urlpatterns += [
